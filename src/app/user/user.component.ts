3a22adcc9598bd6a01841055d87efd8bab397933
Javier Lete <jglete@gmail.com>
2019-04-08 19:19:28 +0200

    Traslado de user a user-list component

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { UserService } from '../services/user.service';
-import { User } from '../modelos/user.model';
 
 @Component({
   selector: 'app-user',
@@ -9,14 +7,9 @@ import { User } from '../modelos/user.model';
 })
 export class UserComponent implements OnInit {
 
-  users: User[];
-  
-  constructor(private userService: UserService) { }
+  constructor() { }
 
   ngOnInit() {
-    this.userService.get().subscribe(
-      users => this.users = users
-    );
   }
 
 }
