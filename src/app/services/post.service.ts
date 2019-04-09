787b5fe2a397b2dc206e78fa70900f336aeca712
Javier Lete <jglete@gmail.com>
2019-04-09 20:58:29 +0200

    Servicios reales

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -2,24 +2,28 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { POSTS } from '../mocks/post.mock';
 import { Post } from '../modelos/post.model';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
 
-  constructor() { }
+  servidor = 'http://localhost:3000';
+  url = '/posts/';
+
+  constructor(private http: HttpClient) { }
 
   get(): Observable<Post[]> {
-    return of(POSTS);
+    return this.http.get<Post[]>(this.servidor + this.url);
   }
 
   getByUserId(id: number): Observable<Post[]> {
-    return of(POSTS.filter(post => post.userId === id));
+    return this.http.get<Post[]>(this.servidor + '/users/' + id + this.url);
   }
 
   getById(id: number): Observable<Post> {
-    return of(POSTS.filter(post => post.id === id).slice()[0]);
+    return this.http.get<Post>(this.servidor + this.url + id);
 
   }
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,16 +2,23 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { User } from '../modelos/user.model';
 import { USERS } from '../mocks/user.mock';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+
+  servidor = 'http://localhost:3000';
+  url = '/users/';
+
+  constructor(private http: HttpClient) {}
+
   get(): Observable<User[]> {
-    return of(USERS);
+    return this.http.get<User[]>(this.servidor + this.url);
   }
 
   getById(id: number): Observable<User> {
-    return of(USERS.filter(user => user.id === id).slice()[0]);
+    return this.http.get<User>(this.servidor + this.url + id);
   }
 }
